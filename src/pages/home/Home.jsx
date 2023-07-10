4ad610bed1bcee63c133a4f80229d791c642e82b
Şeymanur Çürükcü <seymanurcurukcu@gmail.com>
2023-07-10 16:11:24 +0300

    currency chart added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Content1 from '../../components/content1/Content1';
 import Content2 from '../../components/content2/Content2';
 import Content3 from '../../components/content3/Content3';
+import ChartsButtons from '../../components/graphcontent/ChartsButtons';
 
 const Home = () => {
+ 
   return (
     <>
       <div className='mb-4'>
@@ -11,6 +13,9 @@ const Home = () => {
       </div>
       <div className='py-4'>
         <Content2 />
+      </div>
+      <div>
+        <ChartsButtons/>
       </div>
       <div className='py-4'>
         <Content3 />
