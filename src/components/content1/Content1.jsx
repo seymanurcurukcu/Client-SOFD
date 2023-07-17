a1e59df16798a8a60c1a6c9696906a1b127ac415
Gulben Emiroglu <gulben.emiroglu@gmail.com>
2023-07-17 10:55:35 +0300

    deploy workflow

--- src/components/content1/Content1.jsx
+++ src/components/content1/Content1.jsx
@@ -1,54 +1,37 @@
 import React from 'react'
 import { Carousel, Container } from 'react-bootstrap';
-import pic1 from '../../../public/man-hands-using-computers-analyze-data-investment-charts-financial-trading-plans-digital-banking-technology-marketing.jpg'
-import pic2 from '../../../public/site 1.png'
-import pic3 from '../../../public/site 2.png'
+import pic1 from '../../assets/img/carouselImg/1.png'
+import pic2 from '../../assets/img/carouselImg/2.png'
+import pic3 from '../../assets/img/carouselImg/3.png'
 import './content1.css'
 
+const images = [
+    {
+        carouselImg: pic1
+    },
+    {
+        carouselImg: pic2
+    },
+    {
+        carouselImg: pic3
+    }
+]
 
 
-const Content1 = () => {
+const Content1 = (props) => {
     return (
         <Container fluid>
             <Carousel >
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={pic1}
-                        alt="First slide"
-                        
-                    />
-                    {/* <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption> */}
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={pic2}
-                        alt="Second slide"
-                    />
-
-                    {/* <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption> */}
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={pic3} 
-                        alt="Third slide"
-                    />
-
-                    {/* <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption> */}
-                </Carousel.Item>
+                {images.map((image, index) => (
+                    <Carousel.Item key={index}>
+                        <img
+                            className="d-block w-100"
+                            src={image.carouselImg}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                ))}
+           
             </Carousel>
         </Container>
     )
